190e7cdd50c04e621a407223f710e4157fbdf738
Marina <marinafriendrix@gmail.com>
2022-08-15 15:16:57 +0300

    задача 2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+Console.Write("Введите координату x1: ");
+ int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату z1: ");
+ int z1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Введите координату x2: ");
+ int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату z2: ");
+ int z2 = Convert.ToInt32(Console.ReadLine());
+
+
+ double d = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+
+ Console.WriteLine($"{d:f3}"); //это указание порядка после запятой
+ 
+
+
+
+
+
