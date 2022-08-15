0ac09c1febf63697434fcfd3d209b7e078ef0293
Marina <marinafriendrix@gmail.com>
2022-08-15 21:30:50 +0300

    задача 3

--- Program.cs
+++ Program.cs
@@ -2,32 +2,45 @@
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-Console.Write("Введите координату x1: ");
- int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату x1: ");
+//  int x1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите координату y1: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите координату z1: ");
- int z1 = Convert.ToInt32(Console.ReadLine());
-
-
-Console.Write("Введите координату x2: ");
- int x2 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите координату y2: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите координату z2: ");
- int z2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату z1: ");
+//  int z1 = Convert.ToInt32(Console.ReadLine());
 
 
- double d = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+// Console.Write("Введите координату x2: ");
+//  int x2 = Convert.ToInt32(Console.ReadLine());
 
- Console.WriteLine($"{d:f3}"); //это указание порядка после запятой
- 
+// Console.Write("Введите координату y2: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите координату z2: ");
+//  int z2 = Convert.ToInt32(Console.ReadLine());
 
 
+//  double d = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2));
+
+//  Console.WriteLine($"{d:f3}"); //это указание порядка после запятой
 
 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+
+        // Console.Write("Введите число: ");
+        // int num1 = Convert.ToInt32(Console.ReadLine());
+        
+
+        // for (int i = 1; i <= num1; i++)
+        // {
+        //     Console.WriteLine(i*i*i);
+        // }
+        
+
+        
+        
+        
+           
